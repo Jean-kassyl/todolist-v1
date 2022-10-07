5530c3947db1d8af143c246c56635ab3288b80bd
Jean-kassyl <kassylmac@gmail.com>
2022-10-07 16:06:38 +0100

    feat: implementing logic to make the app use mongodb

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 const app = express()
 const currentDate = require(__dirname + '/date.js');
 
@@ -7,21 +8,108 @@ app.set("view engine", "ejs")
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: true}))
 
-// global variables 
+// mongoose connection
 
-const holders = {
-    items: ["check for a great 5 stars food recipee", "buy the raw ingredients"],
+mongoose.connect('mongodb://localhost:27017/todoDB')
+
+
+const holders = {};
+
+// defining the schema of my todos
+
+const singleTodoSchema = new mongoose.Schema({
+    _id: {
+        type: Number,
+        required: [true, "each todo would need an id"]
+    },
+    content: {
+        type: String,
+        required: [true, "you need to enter a todo"]
+    }
+})
+
+
+const saveTodos = (items, singleItem) => {
+   
+
+    let id = holders.hasOwnProperty(items)?holders[items].length + 1: 1;
+    const Todo = mongoose.model(items, singleTodoSchema);
+    const todo = new Todo({
+        _id: id,
+        content: singleItem
+    })
+
+    //todo.save()
+
+    Todo.find({}, function(err, result){
+        if(err){
+            console.log(err)
+        }else {
+            console.log(result)
+        }
+    })
+
+    
+    
+
 }
 
 
 
 app.get('/', function(req, res){
-    res.render('List', {date: currentDate, todos: holders.items, title: 'home' })
+    let items = []
+    const Todo = mongoose.model("homeitems", singleTodoSchema)
+    Todo.find({}, function(err, result){
+            if(err){
+                console.log(err)
+            }else {
+                if(result.length > 0){
+                    console.log("result 1", result)
+                    holders["homeitems"] = result
+                }
+            }
+    })
+    console.log("outside 2")
+    if(holders.hasOwnProperty("homeitems")){
+        items = holders["homeitems"]
+        console.log(items)
+    }
+    res.render('List', {date: currentDate, todos: [], title: 'home' })
 })
 
 app.post('/', (req, res) => {
-    const newTodo = req.body.newTodo;
-    holders.items.push(newTodo)
+    const todoData = req.body.newTodo;
+    
+
+    //here goes the code
+
+
+
+
+    let id = holders.hasOwnProperty("homeitems")?holders["homeitems"].length + 1: 1;
+    const Todo = mongoose.model("homeitems", singleTodoSchema);
+    const todo = new Todo({
+        _id: id,
+        content: todoData
+    })
+
+    todo.save()
+
+
+
+
+
+    // end it here
+
+
+
+
+
+
+
+
+
+
     res.redirect('/')
 })
 
