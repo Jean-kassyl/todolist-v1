6a56e78e4be1decae81114f8b40cb724308f6efa
Jean-kassyl <kassylmac@gmail.com>
2022-10-10 23:35:33 +0100

    feat: impement line through so that it will persist after refresh, using localstorage

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,62 @@
 const items = document.querySelectorAll('.item')
 const radios = document.querySelectorAll('.item input')
-const colorsChanger = document.querySelectorAll('.colors span');
+const colorsChanger = document.querySelectorAll('.colors .color-btn');
+const mobileMenu = document.querySelector('.colors .mobile-menu');
+const aside = document.querySelector('.aside');
 
-radios.forEach(radio => {
-    radio.checked = false;
+
+let check = localStorage.getItem('checker') || [];
+
+
+
+items.forEach(item => {
+    const link = item.querySelector('a').href;
+    if(check.length > 0){
+
+        check.split(',').forEach(url => {
+            if(link === url){
+              let rad =  item.querySelector("input");
+              rad.checked = true;
+    
+              if(rad.checked){
+                item.style.textDecoration = "line-through";
+              }else {
+                item.style.textDecoration = "none";
+              }
+            }
+        })
+
+    }
+  
 })
 
+let urls = []
 items.forEach(item => {
     item.onclick = function(e){
         let input = item.querySelector('input')
         
         if(!input.checked){
             input.checked = true;
-            item.style.textDecoration = "line-through"
+            item.style.textDecoration = "line-through";
+            const url = e.currentTarget.children[2].href
+            
+            urls.includes(url) || urls.push(url);
+            console.log(urls);
+            
+            localStorage.setItem('checker', urls)
         }
         else {
             input.checked = false;
-            item.style.textDecoration = "none"
+            item.style.textDecoration = "none";
+            const url = e.currentTarget.children[2].href
+            if(urls.includes(url)){
+                let ind = urls.indexOf(url)
+                urls.splice(ind, 1)
+            }
+            console.log(urls)
+            localStorage.setItem('checker', urls)
         }
+       
         
        // 
         //item.style.textDecoration = "line-through"
@@ -39,4 +78,9 @@ colorsChanger.forEach(changer => {
             document.body.classList.toggle(color)
         }
     }
+})
+
+
+mobileMenu.addEventListener('click', function(){
+    aside.classList.toggle('mobile');
 })
